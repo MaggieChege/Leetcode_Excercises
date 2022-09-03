01bb9e3f14aa9ceb821a31c6c33251d7e61956e3
Maggie Chege <shikooh.maggie@gmail.com>
2022-09-03 20:49:37 +0300

    Leetcode complete #TODO from yesterday

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -6,11 +6,16 @@ it reads the same forward and backward. Alphanumeric characters include letters 
 Given a string s, return true if it is a palindrome, or false otherwise.
 """
 
+import re
+
 
 class Solution:
     def isPalindrome(self, s: str, target: bool) -> bool:
-        s=s.lower()
-        if s == s[::-1]:
+        s = s.lower()
+        s = s.replace(" ", "")
+        new_string = re.sub(r"[^\w\s]", "", s)
+
+        if new_string == new_string[::-1]:
             return True
         return False
 
@@ -18,4 +23,4 @@ class Solution:
 if __name__ == "__main__":
     s = Solution()
     print(s.isPalindrome(s="race a car", target=False))
-    print(s.isPalindrome(s="A man, a plan, a canal: Panama", target=True)) #TODO
+    print(s.isPalindrome(s="A man, a plan, a canal: Panama", target=True))
